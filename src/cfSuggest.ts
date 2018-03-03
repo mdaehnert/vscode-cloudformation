f527e4230599f9783bcaf124dae4aaaf08f568c1
Michael Dähnert <michael.daehnert@gmail.com>
2018-03-03 10:40:52 +0100

    Codecandy

--- src/cfSuggest.ts
+++ src/cfSuggest.ts
@@ -4,12 +4,12 @@ import vscode = require('vscode');
 
 export class CfCompletionItemProvider implements vscode.CompletionItemProvider {
 
-	public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
-		console.log("nuescht gibts.");
-		
-		return new Promise((resolve, reject) => {
+  public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
+    console.log("Nothing here...");
 
-		});
-	}
+    return new Promise((resolve, reject) => {
+
+    });
+  }
 }
 
